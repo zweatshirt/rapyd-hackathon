8d2ec5d2ad10e48c470b21d8f3d8ac6e4780bf33
zweatshirt <>
2021-06-12 21:33:21 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// configure dotenv to grab MangoDB connection string
+dotenv.config();
 
 const app = express();
 
@@ -9,8 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+
+// grab connection uri from .env file
 const mongoUri = process.env.MONGO_URI;
 
+// example of connecting to DB
 mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('DB connection'))
   .catch((err) => console.log('Failure to connect: ' + err));
