fcc018656fbebd18f3acd66f7055f008f4c6b767
zweatshirt <>
2021-06-18 02:57:56 -0500

    adding more code, I need to fix an issue with useDispatch() and redux, it's complaining about a misused hook

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -4,41 +4,57 @@ import LockIcon from '@material-ui/icons/Lock';
 import Input from './Input';
 import { GoogleLogin } from 'react-google-login';
 import useStyles from './styles';
+import { useHistory } from 'react-router-dom'
 import dotenv from 'dotenv';
-
+import { useDispatch } from 'react-redux';
 
 export const Signup = () => {
-    dotenv.config({ path: 'client/.env' });
-    const client_id = process.env.REACT_APP_CLIENT_ID;
-    const classes = useStyles;
     const [isSignup, setIsSignup] = useState(false);
-
     const [showPassword, setShowPassword] = useState(false);
-    // toggles password visibility on and off
+    
+    dotenv.config({ path: 'client/.env' });
+
+    // client ID for Google OAuth
+    const client_id = process.env.REACT_APP_CLIENT_ID;
+
+    const classes = useStyles;
+    const history = useHistory();
+    // FIX:
+    const dispatch = useDispatch;
+
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
-    const handleChange = () => {
-
-
+    const switchMode = () => {
+        setIsSignup((prev) => !prev);
+        handleShowPassword(false);
     }
     const handleSubmit = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup)
     }
 
-    const googleSuccess = () => {
-    }
-    const googleFailure = () => {
-        console.log("Google sign in failed. Please try again later.");
+    const handleChange = () => {
     }
 
-    const switchMode = () => {
-        setIsSignup((prev) => !prev);
-        handleShowPassword(false);
+    // callbacks to handle on success and failure events
+    // specifically related to Google sign-in/sign-up
+    const googleSuccess = async (res) => {
+        const profObj = res?.profileObj;
+        const token = res?.tokenId;
+        try {
+            dispatch({'type': 'AUTH', data: {profObj, token}});
+            history.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const googleFailure = (error) => {
+        console.log(error);
+        console.log("Google sign in failed. Please try again later.");
     }
 
     return (
         <Container component="main" maxWidth="sm">
             <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
+                 <Avatar className={classes.avatar}>
                     <LockIcon />
                 </Avatar>
                 <Typography variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
